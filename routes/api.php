4d78da56249e72bc7379507403069b59636025a4
MD.SADMAN SHAKIB <gitshochcho@gmail.com>
2025-07-29 12:47:28 +0600

    modify don

--- routes/api.php
+++ routes/api.php
@@ -10,20 +10,11 @@ Route::get('/user', function (Request $request) {
 
 // Appointment API Routes
 
-    // Get all appointments for authenticated user
-    Route::get('/appointments', [HomeController::class, 'getAppointments']);
 
     // Create new appointment
     Route::post('/appointments', [HomeController::class, 'createAppointment']);
 
-    // Get specific appointment
-    Route::get('/appointments/{id}', [HomeController::class, 'getAppointment']);
 
-    // Update appointment (only if pending)
-    Route::put('/appointments/{id}', [HomeController::class, 'updateAppointment']);
-
-    // Cancel appointment (only if pending)
-    Route::delete('/appointments/{id}', [HomeController::class, 'cancelAppointment']);
 
     // Get available doctors
     Route::get('/doctors', [HomeController::class, 'getAvailableDoctors']);
